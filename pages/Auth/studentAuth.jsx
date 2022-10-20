e1fdd7b73fbca8645abc3743870d1eeddb580598
Tharunkumar001 <tharung1411@gmail.com>
2022-10-20 19:30:28 +0530

    complete ft st loginpage

--- pages/Auth/studentAuth.jsx
+++ pages/Auth/studentAuth.jsx
@@ -2,6 +2,7 @@ import styles from '../../styles/Home.module.css'
 import { Card, CardActions, TextField, ButtonBase, createTheme} from '@material-ui/core';
 import Arrow from "@material-ui/icons/SendOutlined"
 import { useState } from 'react';
+import cogoToast from 'cogo-toast';
 
 const theme = createTheme({
     status: {
@@ -21,11 +22,12 @@ const theme = createTheme({
 
 const StudentAuth = () => {
     const [details, setDetails] = useState({rollNo: "", dept: ""});
-    const [error, setError] = useState({rollNo: 'primary', dept: 'primary'});
+    const [err, setErr] = useState({rollNo: 'primary', dept: 'primary'});
 
     const loginHandler = async () => {    
-        
+        //api call
     }
+
     return (
         <div>
         <h1 style={{textAlign:"center", padding:"1rem"}}>#for logo</h1>
@@ -37,13 +39,16 @@ const StudentAuth = () => {
                 <TextField id="RollNo" label="RollNo" variant="filled" helperText="Eg: 19CSR116" 
                 value={details.rollNo.trim()}
                 onChange={(e) => setDetails({...details, rollNo: e.target.value})}
-                color={error}
+                color={err.rollNo}
+                autoFocus={true}
+                inputProps={{ maxLength: 8 }}
                 />
 
-                <TextField id="Department" label="Department" variant="filled" helperText="CSE"
+                <TextField id="Department" label="Department" variant="filled" helperText="Eg: CSE"
                 value={details.dept.trim()}
                 onChange={(e) => setDetails({...details, dept: e.target.value})}
-                color={error}
+                color={err.dept}
+                inputProps={{ maxLength: 5 }}
                 />
             </CardActions>
 
