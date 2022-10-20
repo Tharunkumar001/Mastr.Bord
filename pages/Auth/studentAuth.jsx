cf6922c21b54fd810ab1c284153745d00c02e1e5
Tharunkumar001 <tharung1411@gmail.com>
2022-10-20 19:01:04 +0530

    add functionality on student page

--- pages/Auth/studentAuth.jsx
+++ pages/Auth/studentAuth.jsx
@@ -1,10 +1,31 @@
 import styles from '../../styles/Home.module.css'
-import { Card, CardActions, TextField, Box, CardActionArea, Button, CardContent, ButtonBase } from '@material-ui/core';
-import Image from 'next/image';
+import { Card, CardActions, TextField, ButtonBase, createTheme} from '@material-ui/core';
 import Arrow from "@material-ui/icons/SendOutlined"
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import { useState } from 'react';
+
+const theme = createTheme({
+    status: {
+        danger: '#e53e3e',
+    },
+    palette: {
+        primary: {
+            main: '#0971f1',
+            darker: '#053e85',
+        },
+        neutral: {
+            main: '#64748B',
+            contrastText: '#fff',
+        },
+    },
+});
 
 const StudentAuth = () => {
+    const [details, setDetails] = useState({rollNo: "", dept: ""});
+    const [error, setError] = useState({rollNo: 'primary', dept: 'primary'});
+
+    const loginHandler = async () => {    
+        
+    }
     return (
         <div>
         <h1 style={{textAlign:"center", padding:"1rem"}}>#for logo</h1>
@@ -12,42 +33,29 @@ const StudentAuth = () => {
         <Card className={styles.profileLoginCard}>
         <h1 style={{textAlign:"center"}}>Student</h1>
             <CardActions className={styles.profileLoginCardAction}>
-                
+
                 <TextField id="RollNo" label="RollNo" variant="filled" helperText="Eg: 19CSR116" 
+                value={details.rollNo.trim()}
+                onChange={(e) => setDetails({...details, rollNo: e.target.value})}
+                color={error}
                 />
 
-                <TextField id="Password" label="Password" variant="filled" helperText="Specified Password"
+                <TextField id="Department" label="Department" variant="filled" helperText="CSE"
+                value={details.dept.trim()}
+                onChange={(e) => setDetails({...details, dept: e.target.value})}
+                color={error}
                 />
             </CardActions>
 
             <CardActions className={styles.profileCardContent}>
                 <ButtonBase
                     className={styles.profileSubmitBtn} 
-                    
+                    onClick = {loginHandler}
                 >
                     Login <Arrow /></ButtonBase>
             </CardActions>
 
-            <ButtonBase >ForgotPassword</ButtonBase>
         </Card><br />
-
-                <Dialog
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                >
-                <DialogTitle className={styles.alertDialogTitle}>
-                    Password Recovery!!
-                </DialogTitle>
-                <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button  autoFocus>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
         </div>
     )
 }
