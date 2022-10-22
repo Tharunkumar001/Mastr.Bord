bccebaf53df060a1b7edc91826ad0dea146450a0
Tharunkumar001 <tharung1411@gmail.com>
2022-10-22 10:06:28 +0530

    form validation

--- pages/Auth/adminAuth.jsx
+++ pages/Auth/adminAuth.jsx
@@ -1,8 +1,9 @@
 import styles from '../../styles/Home.module.css'
-import { Card, CardActions, TextField, Box, CardActionArea, Button, CardContent, ButtonBase, makeStyles } from '@material-ui/core';
+import { Card, CardActions, TextField, Button, CardContent, ButtonBase, makeStyles } from '@material-ui/core';
 import Arrow from "@material-ui/icons/SendOutlined"
 import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 import { useState } from 'react';
+import cogoToast from 'cogo-toast';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -21,6 +22,37 @@ const AdminPage = () => {
 
     const loginHandler = async () => {    
         //api call
+        let val = await validate();
+        
+        if(val){
+            //api vall
+        }
+    }
+
+    const validate = () => {
+        //validate details
+        
+        var verifyEmail = String(details.email)
+            .toLowerCase()
+            .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+
+        var verifyUsername = String(details.Username)
+            .match(
+            /^[A-Za-z][A-Za-z0-9_]{7,15}$/ //Tharun1411 
+        );
+
+        var verifyPassword = String(details.Password)
+            .match(
+            /^[A-Z][A-Za-z0-9]/ //Tharun#1411
+        );
+
+        if(verifyEmail | verifyPassword | verifyUsername == null ){
+            cogoToast.error("Enter valid details");
+        }else{
+            return true;
+        }
     }
 
     return (
@@ -35,23 +67,22 @@ const AdminPage = () => {
                         onChange={(e) => setDetails({...details, Email: e.target.value})}
                         color={err.rollNo}
                         autoFocus={true}
-
                     />
 
-                    <TextField id="Username" label="Username" variant="filled" helperText="Specified Username"
+                    <TextField id="Username" label="Username" variant="filled" helperText="Eg: Tharun1411"
                         value={details.Username.trim()}
                         onChange={(e) => setDetails({...details, Username: e.target.value})}
                         color={err.rollNo}
                         autoFocus={true}
-                        inputProps={{ maxLength: 10 }}
+                        inputProps={{ maxLength: 15 }}
                     />
 
-                    <TextField id="Password" label="Password" variant="filled" helperText="Specified Password"
+                    <TextField id="Password" label="Password" variant="filled" helperText="Eg: Tharun#1411"
                         value={details.Password.trim()}
                         onChange={(e) => setDetails({...details, Password: e.target.value})}
                         color={err.rollNo}
                         autoFocus={true}
-                        inputProps={{ maxLength: 10 }}
+                        inputProps={{ maxLength: 15 }}
                     />
                 </CardActions>
 
@@ -78,7 +109,7 @@ const AdminPage = () => {
             </DialogContentText>
             </DialogContent>
             <DialogActions>
-                <Button  autoFocus>
+                <Button autoFocus>
                     Close
                 </Button>
             </DialogActions>
