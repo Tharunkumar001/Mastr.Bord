f49384df71899bd8dcba494420034e7ffb80b473
Tharunkumar001 <tharung1411@gmail.com>
2022-10-27 21:04:09 +0530

    flip card

--- pages/card/card.jsx
+++ pages/card/card.jsx
@@ -1,4 +1,5 @@
-import { Card, CardHeader } from "@material-ui/core";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+import { PersonAdd } from "@material-ui/icons";
 import { makeStyles, StylesContext } from "@material-ui/styles";
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
@@ -6,7 +7,25 @@ import ReactCardFlip from "react-card-flip";
 const useStyles = makeStyles((theme) => ({
     rootCard:{
         width: '250px',
-        height: '400px'
+        height: '400px',
+        borderRadius: '0.5rem',
+        background: "linear-gradient(107deg, rgba(43,41,44,1) 0%, rgba(59,135,218,1) 0%, rgba(240,242,244,1) 100%, rgba(82,111,143,0) 100%)",
+
+    },
+
+    content:{
+        
+    },
+
+    innerCard:{
+        width: '220px',
+        height: '350px',
+        padding: '0.5rem'
+    },
+
+    iconHeader:{
+        display: 'flex',
+        flexDirection: 'row'
     }
 }))
 
@@ -20,7 +39,22 @@ const FlipCard = () => {
                 flipDirection="vertical">
                     
                     <Card className={classes.rootCard}>
-                        <h1>front</h1>
+                        <CardContent className={classes.content}>
+                            <Card className={classes.innerCard}>
+                                <CardContent className={classes.iconHeader}>
+                                    <h2>@username</h2>
+                                </CardContent>
+
+                                <CardContent>
+                                    <h3>22</h3>
+                                    <span>present Days</span>
+
+                                    <h4>total days</h4>
+                                    <h4>total days</h4>
+                                    <h4>total days</h4>
+                                </CardContent>
+                            </Card>
+                        </CardContent>
                     </Card>
 
                     <Card className={classes.rootCard}>
