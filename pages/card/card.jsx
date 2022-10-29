1f92395b38699fae91c437a30d70697b428b0218
Tharunkumar001 <tharung1411@gmail.com>
2022-10-29 20:34:02 +0530

    card design

--- pages/card/card.jsx
+++ pages/card/card.jsx
@@ -1,11 +1,13 @@
 import { Card, CardContent, CardHeader, CardMedia } from "@material-ui/core";
 import { makeStyles, StylesContext } from "@material-ui/styles";
+import Image from "next/image";
 import backgroundImage from "../../public/cardBack.jpg";
 
 const useStyles = makeStyles((theme) => ({
     rootCard:{
+        backgroundImage:`url(${backgroundImage})`,
         width:'250px', 
-        height:'50vh',
+        height:'70vh',
     }
 }))
 
@@ -15,11 +17,17 @@ const FlipCard = () => {
     return (
         <div className={classes.root}>
             <Card className={classes.rootCard}>
+                <CardMedia
+                    component="img"
+                    height="114"
+                    image="/cardBack.jpg"
+                    alt="Paella dish"
+                />
                 <CardHeader
                     title="Shrimp and Chorizo Paella"
                     subheader="September 14, 2016"
                 />
-                </Card>
+            </Card>
         </div>
     );
 }
