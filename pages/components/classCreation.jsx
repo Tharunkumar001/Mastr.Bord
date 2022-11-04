0e067ae8593ff6efd0de480070d33f69b6d7ec34
Tharunkumar001 <tharung1411@gmail.com>
2022-11-04 20:45:59 +0530

    class creation form

--- pages/components/classCreation.jsx
+++ pages/components/classCreation.jsx
@@ -1,10 +1,20 @@
-import { Card, CardHeader, makeStyles } from "@material-ui/core";
+import { Card, CardHeader, makeStyles, MenuItem, TextField } from "@material-ui/core";
 import { useState } from "react";
 import styles from "../../styles/Home.module.css";
 
 import React from 'react';
 import RangeSlider from 'react-bootstrap-range-slider';
 
+const currencies = [
+    {
+        value: 'CSE',
+        label: 'CSE',
+    },
+    {
+        value: 'IT',
+        label: 'IT',
+    },
+    ];
 
 const useStyles = makeStyles((theme) => ({
     pairOne:{
@@ -26,8 +36,14 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const ClassCreation = () => {
+
     const classes = useStyles();
     const [ value, setValue ] = useState(10); 
+    const [form, setForm] = React.useState({count:0, dept:'CSE', startRno:1, Section:'A', YearOfStudy:4, });
+
+    const handleChange = (event) => {
+        setCurrency(event.target.value);
+    };
 
     return(
         
@@ -37,75 +53,65 @@ const ClassCreation = () => {
                 <div className={classes.form}>
                     <form>
                         <div className={classes.pairOne}>
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                                <TextField id="outlined-basic" label="Count" variant="outlined" />
+                            </div>
                             
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                                <TextField id="outlined-basic" label="Starting R.NO" variant="outlined" />
+                            </div>
                         </div>
                         
                         <div className={classes.pairOne}>
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                            <TextField
+                                    id="outlined-basic"
+                                    select
+                                    label="Select"
+                                    value={currency}
+                                    onChange={() => {
+                                        
+                                    }}
+                                    helperText="Please select your department"
+                                    variant="outlined"
+                                >
+                                {currencies.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>                            
+                            </div>
                             
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                            <TextField
+                                    id="outlined-basic"
+                                    select
+                                    label="Select"
+                                    value="ODD/EVEN"
+                                    onChange={() => {
+
+                                    }}
+                                    helperText="Please select sem details"
+                                    variant="outlined"
+                                >
+                                {["ODD","EVEN"].map((option,index) => (
+                                    <MenuItem key={index} value={option}>
+                                    {option}
+                                    </MenuItem>
+                                ))}
+                            </TextField>     
+                            </div>
                         </div>
 
                         <div className={classes.pairOne}>
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                            </div>
                             
-                            <Card>
-                                <label 
-                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
-                                >Count
-                                <RangeSlider
-                                    value={value}
-                                    onChange={changeEvent => setValue(changeEvent.target.value)}
-                                />
-                                </label>
-                            </Card>
+                            <div>
+                                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                            </div>
                         </div>
                     </form>
                 </div>
