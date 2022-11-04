4b9ca9037a5e372572dab41def6479e9e31b9342
Tharunkumar001 <tharung1411@gmail.com>
2022-11-04 05:30:21 +0530

    working on class creation form

--- pages/components/classCreation.jsx
+++ pages/components/classCreation.jsx
@@ -1,33 +1,113 @@
 import { Card, CardHeader, makeStyles } from "@material-ui/core";
 import { useState } from "react";
-import InputRange from "react-input-range";
 import styles from "../../styles/Home.module.css";
-import 'react-input-range/lib/css/index.css';
+
+import React from 'react';
+import RangeSlider from 'react-bootstrap-range-slider';
+
 
 const useStyles = makeStyles((theme) => ({
-    
+    pairOne:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        gap:'1rem',
+        [theme.breakpoints.up('md')] : {
+            display:"flex",
+            flexDirection:"row",
+            justifyContent:"center",
+            alignItems:"center",
+            gap:'1rem',
+            padding:'1rem'
+        }
+    },
+
 }))
 
 const ClassCreation = () => {
     const classes = useStyles();
-    const [state, setState] = useState(0);
+    const [ value, setValue ] = useState(10); 
 
     return(
         
         <div>
             <Card className={styles.superAdminCardRoot}>
-                <div className={styles.superAdminCard}>
-                <CardHeader title="@creation" subheader="sueperadmin@gmail.com" />
-                    <div className={styles.form}>
-                        <InputRange
-                            maxValue={10}
-                            minValue={0}
-                            value={state}
-                            onChange={value => setState({ value })} 
-                        />
-                    </div>
-                    
-                
+                <h2 style={{textAlign:"center"}}>Class Creation Form</h2>
+                <div className={classes.form}>
+                    <form>
+                        <div className={classes.pairOne}>
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                            
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                        </div>
+                        
+                        <div className={classes.pairOne}>
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                            
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                        </div>
+
+                        <div className={classes.pairOne}>
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                            
+                            <Card>
+                                <label 
+                                    style={{fontStyle:"bold",fontSize:"1.3rem"}}
+                                >Count
+                                <RangeSlider
+                                    value={value}
+                                    onChange={changeEvent => setValue(changeEvent.target.value)}
+                                />
+                                </label>
+                            </Card>
+                        </div>
+                    </form>
                 </div>
             </Card>
         </div>
