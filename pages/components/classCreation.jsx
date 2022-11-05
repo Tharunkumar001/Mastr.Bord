c7e65d96feec2d65e22ba5c8a1fe038bfaac84ae
Tharunkumar001 <tharung1411@gmail.com>
2022-11-05 15:22:27 +0530

    form update

--- pages/components/classCreation.jsx
+++ pages/components/classCreation.jsx
@@ -1,8 +1,9 @@
-import { Card, CardHeader, makeStyles, MenuItem, TextField } from "@material-ui/core";
+import { Box, Card, CardHeader, makeStyles, MenuItem, TextField } from "@material-ui/core";
 import { useState } from "react";
 import styles from "../../styles/Home.module.css";
 
 import React from 'react';
+import * as _ from "lodash";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -15,12 +16,17 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.up('md')] : {
             display:"flex",
             flexDirection:"row",
-            justifyContent:"center",
-            alignItems:"center",
             gap:'1rem',
-            padding:'1rem'
+            padding:'1.5rem',            
         }
     },
+
+    form:{
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+
+    }
 
 }))
 
@@ -34,6 +40,11 @@ const ClassCreation = () => {
         setCurrency(event.target.value);
     };
 
+    const count = _.range(1, 66).map((item) => {
+        return { value: item };
+    });
+
+    const rollNo = [1,60,61,62,63,64,65,66,67,68,69,70]
     return(
         
         <div>
@@ -41,89 +52,111 @@ const ClassCreation = () => {
                 <h2 style={{textAlign:"center"}}>Class Creation Form</h2>
                 <div className={classes.form}>
                     <form>
-                        <div className={classes.pairOne}>
-                            <div>
-                                <TextField id="outlined-basic" label="Count" variant="outlined" />
-                            </div>
-                            
-                            <div>
-                                <TextField id="outlined-basic" label="Starting R.NO" variant="outlined" />
-                            </div>
-                        </div>
-                        
-                        <div className={classes.pairOne}>
-                            <div>
-                            <TextField
-                                    id="outlined-basic"
-                                    select
-                                    label="Select"
-                                    value={form.dept}
-                                    onChange={(e) => setForm({...form, dept: e.target.value})}
-                                    helperText="Please select your department"
-                                    variant="outlined"
-                                >
-                                {['CSE','IT'].map((option,index) => (
-                                    <MenuItem key={index} value={option}>
-                                    {option}
-                                    </MenuItem>
-                                ))}
-                            </TextField>                            
-                            </div>
-                            
-                            <div>
-                            <TextField
-                                    id="outlined-basic"
-                                    select
-                                    label="Select"
-                                    value={form.oddOrEven}
-                                    onChange={(e) => setForm({...form, oddOrEven: e.target.value})}
-                                    helperText="Please select sem details"
-                                    variant="outlined"
-                                >
-                                {["ODD","EVEN"].map((option,index) => (
-                                    <MenuItem key={index} value={option}>
-                                    {option}
-                                    </MenuItem>
-                                ))}
-                            </TextField>     
-                            </div>
-                        </div>
-
-                        <div className={classes.pairOne}>
-                            <div>
-                            <TextField
-                                    id="outlined-basic"
-                                    select
-                                    label="Select"
-                                    value={form.section}
-                                    onChange={(e) => setForm({...form, section: e.target.value})}
-                                    helperText="Please select section of class"
-                                    variant="outlined"
-                                >
-                                {["A","B"].map((option,index) => (
-                                    <MenuItem key={index} value={option}>
-                                    {option}
-                                    </MenuItem>
-                                ))}
-                            </TextField>                                 </div>
-                            
-                            <div>
-                            <TextField
-                                    id="outlined-basic"
-                                    select
-                                    label="Select"
-                                    value={form.yearOfStudy}
-                                    onChange={(e) => setForm({...form, yearOfStudy: e.target.value})}
-                                    helperText="Please select year of study"
-                                    variant="outlined"
-                                >
-                                {[1,2,3,4].map((option,index) => (
-                                    <MenuItem key={index} value={option}>
-                                    {option}
-                                    </MenuItem>
-                                ))}
-                            </TextField>                                 </div>
-                        </div>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& .MuiTextField-root': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                    <div  className={classes.pairOne}>
+                        <TextField
+                            id="outlined-select-currency"
+                            variant="filled"
+                            select
+                            label="Count"
+                            value={form.count}
+                            onChange={(e) => setForm({...form, count:e.target.value})}
+                            helperText="Please select valid details"
+                            >
+                            {count.map((option,ind) => (
+                                <MenuItem key={ind} value={option.value}>
+                                    {option.value}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            id="outlined-select-currency"
+                            variant="filled"
+                            select
+                            label="Starting R.No"
+                            value={form.startRno}
+                            onChange={(e) => setForm({...form, startRno:e.target.value})}
+                            helperText="Please select valid details"
+                        >
+                            {rollNo.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </div>
+                    <div  className={classes.pairOne}>
+                    <TextField
+                        id="outlined-select-currency"
+                        variant="filled"
+                        select
+                        label="Dept"
+                        value={form.dept}
+                        onChange={(e) => setForm({...form, dept:e.target.value})}
+                        helperText="Please select valid details"
+                        >
+                        {['CSE','IT'].map((option) => (
+                            <MenuItem key={option} value={option}>
+                            {option}
+                            </MenuItem>
+                        ))}
+                        </TextField>
+                        <TextField
+                            id="outlined-select-currency"
+                            variant="filled"
+                            select
+                            label="Section"
+                            value={form.section}
+                            onChange={(e) => setForm({...form, section:e.target.value})}
+                            helperText="Please select valid details"
+                        >
+                            {['A','B'].map((option) => (
+                                <MenuItem key={option} value={option}>
+                                {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </div>
+                    <div  className={classes.pairOne}>
+                    <TextField
+                        id="outlined-select-currency"
+                        variant="filled"
+                        select
+                        label="Sem"
+                        value={form.oddOrEven}
+                        onChange={(e) => setForm({...form, oddOrEven:e.target.value})}
+                        helperText="Please select valid details"
+                        >
+                        {['ODD','EVEN'].map((option) => (
+                            <MenuItem key={option} value={option}>
+                            {option}
+                            </MenuItem>
+                        ))}
+                        </TextField>
+                        <TextField
+                            id="outlined-select-currency"
+                            variant="filled"
+                            select
+                            label="Year"
+                            value={form.yearOfStudy}
+                            onChange={(e) => setForm({...form, yearOfStudy:e.target.value})}
+                            helperText="Please select valid details"
+                        >
+                            {_.range(1,5).map((option) => (
+                                <MenuItem key={option} value={option}>
+                                {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </div>
+                    </Box>
                     </form>
                 </div>
             </Card>
